404b78ba5b1e664ea80c8777fc84fa00218c6484
Ruben Anton <rubocoptero@gmail.com>
2016-06-06 08:33:05 +0200

    Generalize returns

--- src/code.rb
+++ src/code.rb
@@ -5,10 +5,28 @@
 # 1989 = MCMLXXXIX
 
 
-$max_reps_of_I = 3
+$max_num_from_I = 3
 
 def to_roman_numeral(number)
-  return 'I' * number if number <= $max_reps_of_I
-  return 'VI' if number > 5
-  return 'I' * (5 - number) + 'V'
+  values = {
+    'I' => 1,
+    'V' => 5
+  }
+
+  main = get_main_roman_numeral_for(number)
+  main_value = values[main]
+
+  if main_value - number > 0
+    ('I' * (main_value - number)) + main
+  else
+    main + ('I' * (number - main_value))
+  end
+end
+
+def get_main_roman_numeral_for(number)
+  max_num_from_I = 3
+  max_num_from_V = 8
+
+  return 'I' if number <= max_num_from_I
+  return 'V' if number <= max_num_from_V
 end
