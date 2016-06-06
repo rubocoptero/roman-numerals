a3579632b52a69416335d0cff5adcd1c53304583
Ruben Anton <rubocoptero@gmail.com>
2016-06-06 08:01:08 +0200

    Unify two guard clauses

--- src/code.rb
+++ src/code.rb
@@ -3,10 +3,11 @@
 # 9 = IX
 # 1066 = MLXVI
 # 1989 = MCMLXXXIX
-#
+
+
+$max_reps_of_I = 3
 
 def to_roman_numeral(number)
-  return 'I' * number if number <= 3
-  return 'IV' if number < 5
-  return 'V'
+  return 'I' * number if number <= $max_reps_of_I
+  return 'I' * (5 - number) + 'V'
 end
